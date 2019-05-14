d08567f6f48d01a82d847de5bca512a1acfa5c0b
Vitaly-V <gtopbox@gmail.com>
2019-05-14 09:51:17 +0300

    Add game logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _BallState();
+  }
+}
+
+class _BallState extends State<Ball> {
+  int currentBallNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        setState(() {
+          currentBallNumber = Random().nextInt(5) + 1;
+        });
+      },
+      child: Center(
+        child: Image.asset('images/ball$currentBallNumber.png'),
+      ),
+    );
+  }
+}
